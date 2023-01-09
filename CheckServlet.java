122d17d20bbb5da286b9bdfe3eb63d60a23344fa
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-09 15:25:30 +0900

    Create CheckServlet.java

---
+++ CheckServlet.java
@@ -0,0 +1,57 @@
+package unit02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CheckServlet
+ */
+@WebServlet("/CheckServlet")
+public class CheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CheckServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String str[] = request.getParameterValues("ch");
+		if (str == null) {
+			out.print("아무것도 선택하지 않았다");
+		} else {
+			for (String s : str) {
+				out.print(s + " ");
+			}
+		}
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
