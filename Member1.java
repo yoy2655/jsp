40040c0598d704f8553c6e455e393cd6c0fc51a1
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-12 15:12:17 +0900

    Create Member1.java

---
+++ Member1.java
@@ -0,0 +1,52 @@
+package unit01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Member
+ */
+@WebServlet("/Member")
+public class Member extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Member() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=urf-8");
+		
+		String id=request.getParameter("id");
+		String password=request.getParameter("pass1");
+		
+		if(id!=null&&password!=null) {
+			HttpSession session=request.getSession();
+			session.setAttribute("id1", id);
+		}
+		response.sendRedirect("session.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
