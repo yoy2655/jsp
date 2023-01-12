a41c2e1d28e13d7b0764ddafe157a1ec98a3abb9
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-12 16:17:16 +0900

    Create StuBean.java

---
+++ StuBean.java
@@ -0,0 +1,39 @@
+package bean;
+
+public class StuBean {
+	private String id;
+	private String pass;
+	private String email;
+	private String tel;
+	private String addr;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+}
