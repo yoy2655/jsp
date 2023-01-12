7b0aa21f12561bcf809a76a3bb36eaa34640de39
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-12 15:52:46 +0900

    Create MemberBean.java

---
+++ MemberBean.java
@@ -0,0 +1,46 @@
+package bean;
+
+public class MemberBean {
+	private String name;
+	private String id;
+	private String nickname;
+	private String pw;
+	private String email;
+	private String phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
