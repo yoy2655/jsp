6f476c7b6c0d2824bfea3f23d9f06e66e5a880d3
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-12 10:45:17 +0900

    Create First.java

---
+++ First.java
@@ -0,0 +1,53 @@
+package unit01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet("/First")
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public First() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		//response.sendRedirect("Second");
+		//First.java에서 돌리면 First가 떠야되는데 
+		//Second.java의 출력문인 "SendRedirect"가 뜨고 있다
+		//sendRedirect는 페이지만 이동한 것이다.
+		
+		response.sendRedirect("Second?name=lee");
+		//값을 넘겨줬을때
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
