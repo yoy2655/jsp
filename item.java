1d1e92b56423ed8913110fcb93771436c389367f
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-13 12:09:30 +0900

    Create item.java

---
+++ item.java
@@ -0,0 +1,31 @@
+package unit01;
+
+public class Item {
+	private String name;
+	private String price;
+	private String desc1;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getDesc1() {
+		return desc1;
+	}
+
+	public void setDesc1(String desc1) {
+		this.desc1 = desc1;
+	}
+}
