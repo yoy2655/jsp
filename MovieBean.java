82765def4c33b0d871b574f340a46f76b0580c3e
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-13 10:30:20 +0900

    Create MovieBean.java

---
+++ MovieBean.java
@@ -0,0 +1,40 @@
+package co.sjw.movie;
+
+public class MovieBean {
+	private String title;
+	private String price;
+	private String director;
+	private String synopsis;
+	private String genre;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getSynopsis() {
+		return synopsis;
+	}
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+}
