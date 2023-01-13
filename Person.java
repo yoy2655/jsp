c3a588155637f9b1ec3e48246c0319ce47e914a9
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-13 11:30:14 +0900

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,41 @@
+package unit01;
+
+public class Person {
+	private String name;
+	private String nic;
+	private String job;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNic() {
+		return nic;
+	}
+
+	public void setNic(String nic) {
+		this.nic = nic;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
