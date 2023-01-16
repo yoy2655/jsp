fcd55ff2424a06a4ec797a5f932e526d604304f2
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-16 14:08:56 +0900

    Create EmployeeBean.java

---
+++ EmployeeBean.java
@@ -0,0 +1,26 @@
+package unit01;
+
+public class Employee {
+
+	private String name;
+	private String address;
+	private String ssn;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+}
