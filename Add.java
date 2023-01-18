19d1849998be4f57674220a86d72d63899b73ae8
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-18 12:05:58 +0900

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Add
+ */
+@WebServlet("/Add")
+public class Add extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Add() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int n1=30;
+		int n2=40;
+		int sum=n1+n2;
+		
+		request.setAttribute("n1", n1);
+		request.setAttribute("n2", n2);
+		request.setAttribute("sum", sum);
+		
+		RequestDispatcher dis=request.getRequestDispatcher("add_el.jsp");
+		dis.forward(request, response);
+		//RequestDispatcher에 담아서 forward 이동
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
