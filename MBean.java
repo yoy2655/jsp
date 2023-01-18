20c8277fe220a6cd777648e623e76255173efd45
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-01-18 13:07:40 +0900

    Update MBean.java

--- MBean.java
+++ MBean.java
@@ -3,26 +3,33 @@ package test;
 public class MBean {
 	private String name;
 	private String id;
-	
+
+	public MBean() {
+	}
+
 	public MBean(String name, String id) {
+		super();
 		this.name = name;
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String toString() {
-		return name+","+id;
+		return name + "," + id;
 	}
 }
