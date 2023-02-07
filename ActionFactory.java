8d069d0ea81b1e718420d9cdff8de05f27ddbef8
yoy2655 <118251009+yoy2655@users.noreply.github.com>
2023-02-07 09:23:47 +0900

    Add files via upload

---
+++ ActionFactory.java
@@ -0,0 +1,50 @@
+package com.controller;
+
+import com.controller.action.Action;
+import com.controller.action.BoardCheckPassAction;
+import com.controller.action.BoardCheckPassFormAction;
+import com.controller.action.BoardDeleteAction;
+import com.controller.action.BoardListAction;
+import com.controller.action.BoardUpdateAction;
+import com.controller.action.BoardUpdateFormAction;
+import com.controller.action.BoardViewAction;
+import com.controller.action.BoardWriteAction;
+import com.controller.action.BoardWriteFormAction;
+
+public class ActionFactory {
+	private static ActionFactory instance = new ActionFactory();
+
+	private ActionFactory() {
+		super();
+	}
+
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+
+	public Action getAction(String command) {
+		Action action = null;
+		System.out.println("ActionFactory :" + command);
+		/* 추가된 부분 */
+		if (command.equals("board_list")) {
+			action = new BoardListAction();
+		} else if (command.equals("board_write_form")) {
+			action = new BoardWriteFormAction();
+		} else if (command.equals("board_write")) {
+			action = new BoardWriteAction();
+		} else if (command.equals("board_view")) {
+			action = new BoardViewAction();
+		} else if (command.equals("board_check_pass_form")) {
+			action = new BoardCheckPassFormAction();
+		} else if (command.equals("board_check_pass")) {
+			action = new BoardCheckPassAction();
+		} else if (command.equals("board_update_form")) {
+			action = new BoardUpdateFormAction();
+		} else if (command.equals("board_update")) {
+			action = new BoardUpdateAction();
+		} else if (command.equals("board_delete")) {
+			action = new BoardDeleteAction();
+		}
+		return action;
+	}
+}
--- BoardDAO.java
+++ BoardDAO.java
@@ -1,276 +1,187 @@
-package test;
+package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Vector;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import util.DBManager;
+
+import com.dto.BoardVO;
 
 public class BoardDAO {
-	Connection con;
-	PreparedStatement pstmt;
-	ResultSet rs;
-
-	public void getCon() {
-		try {
-			Context ctx = new InitialContext();
-			Context envctx = (Context) ctx.lookup("java:comp/env");
-			// 데이터 소스 객체 선언
-			DataSource ds = (DataSource) envctx.lookup("jdbc/pool");
-			con = ds.getConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private BoardDAO() {
 	}
 
-	public void insertBoard(BoardBean bean) {
-		getCon();
+	private static BoardDAO instance = new BoardDAO();
 
-		int ref = 0;
-		int re_step = 1;
-		int re_level = 1;
-
-		try {
-			String rsql = "select max(ref) from board"; // 최신글먼저 가져오기 위한 작업
-			pstmt = con.prepareStatement(rsql);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				ref = rs.getInt(1) + 1;
-			}
-
-			String sql = "insert into board values(bo_seq.NEXTVAL,?,?,?,?,sysdate,?,?,?,0,?)";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, bean.getWriter());
-			pstmt.setString(2, bean.getEmail());
-			pstmt.setString(3, bean.getSubject());
-			pstmt.setString(4, bean.getPassword());
-			pstmt.setInt(5, ref);
-			pstmt.setInt(6, re_step);
-			pstmt.setInt(7, re_level);
-			pstmt.setString(8, bean.getContent());
-
-			pstmt.executeUpdate();
-			con.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public static BoardDAO getInstance() {
+		return instance;
 	}
 
-	public int getAllCount() {
-		getCon();
-
-		// 게시글 전체수
-		int count = 0;
-
+	public List<BoardVO> selectAllBoards() {
+		String sql = "select * from board order by num desc";
+		List<BoardVO> list = new ArrayList<BoardVO>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		try {
-			String sql = "select count(*) from board";
-			pstmt = con.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				count = rs.getInt(1);
-			}
-			con.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return count;
-	}
-
-	public Vector<BoardBean> allBoard(int start, int end) {
-		// 글쓰기: 여러분의 정보를 입력함 -> BoardBean에 저장됨(useBean)
-		// useBean에 저장했떤 값은 꺼내와서 테이블에 삽입
-		Vector<BoardBean> v = new Vector<>();
-
-		getCon();
-
-		try {
-			String sql="select * from (select A.*, Rownum Rnum from"
-					+ "(select * from board order by ref desc, re_level asc)A)"
-					+ "where Rnum>=? and Rnum<=?";;
-
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, start);
-			pstmt.setInt(2, end);
-			rs = pstmt.executeQuery();
-
+			conn = DBManager.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
 			while (rs.next()) {
-				// 13명의 글이 db에 저장되어있다.저장되어 있는 값을 (자바빈)에 저장할거임 -> 벡터에 저장
-				BoardBean bean = new BoardBean();
-				bean.setNum(rs.getInt(1));
-				bean.setWriter(rs.getString(2));
-				bean.setEmail(rs.getString(3));
-				bean.setSubject(rs.getString(4));
-				bean.setPassword(rs.getString(5));
-				bean.setReg_date(rs.getDate(6).toString());// 객체를 문자열로
-				bean.setRef(rs.getInt(7));
-				bean.setRe_step(rs.getInt(8));
-				bean.setRe_level(rs.getInt(9));
-				bean.setReadcount(rs.getInt(10));
-				bean.setContent(rs.getString(11));
-
-				v.add(bean);
+				BoardVO bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setReadcount(rs.getInt("readcount"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+				list.add(bVo);
 			}
-			con.close();
-
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, stmt, rs);
 		}
-		return v;
+		return list;
 	}
 
-	public BoardBean oneBoard(int num) {
-		BoardBean bean = new BoardBean();
-
-		getCon();
+	public void insertBoard(BoardVO bVo) {
+		String sql = "insert into board("
+				+ "num, name, email, pass, title, content) "
+				+ "values(bo_seq.nextval, ?, ?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		try {
-			String readsql = "update board set readcount=readcount+1 where num=?";
-			pstmt = con.prepareStatement(readsql);
-			pstmt.setInt(1, num);
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bVo.getName());
+			pstmt.setString(2, bVo.getEmail());
+			pstmt.setString(3, bVo.getPass());
+			pstmt.setString(4, bVo.getTitle());
+			pstmt.setString(5, bVo.getContent());
 			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
 
-			String sql = "select * from board where num=?";
-			// DB에 값을 가져와 브라우저에 뿌려야함
+	public void updateReadCount(String num) {
+		String sql = "update board set readcount=readcount+1 where num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
 
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, num);
-
+	// 게시판 글 상세 내용 보기 :글번호로 찾아온다. : 실패 null,
+	public BoardVO selectOneBoardByNum(String num) {
+		String sql = "select * from board where num = ?";
+		BoardVO bVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				bean.setNum(rs.getInt(1));
-				bean.setWriter(rs.getString(2));
-				bean.setEmail(rs.getString(3));
-				bean.setSubject(rs.getString(4));
-				bean.setPassword(rs.getString(5));
-				bean.setReg_date(rs.getDate(6).toString());// 객체를 문자열로
-				bean.setRef(rs.getInt(7));
-				bean.setRe_step(rs.getInt(8));
-				bean.setRe_level(rs.getInt(9));
-				bean.setReadcount(rs.getInt(10));
-				bean.setContent(rs.getString(11));
+				bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+				bVo.setReadcount(rs.getInt("readcount"));
 			}
-			con.close();
-
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
 		}
-		return bean;
+		return bVo;
 	}
 
-	public void reWriteBoard(BoardBean bean) {
-		int ref = bean.getRef();// 1
-		int re_step = bean.getRe_step(); // 2
-		int re_level = bean.getRe_level(); // 2
-
-		getCon();
+	public void updateBoard(BoardVO bVo) {
+		String sql = "update board set name=?, email=?, pass=?, "
+				+ "title=?, content=? where num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		try {
-			String levelsql = "update board set re_level=re_level+1 where ref=? and re_level>?";
-
-			pstmt = con.prepareStatement(levelsql);
-			pstmt.setInt(1, ref);
-			pstmt.setInt(2, re_level);
-
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bVo.getName());
+			pstmt.setString(2, bVo.getEmail());
+			pstmt.setString(3, bVo.getPass());
+			pstmt.setString(4, bVo.getTitle());
+			pstmt.setString(5, bVo.getContent());
+			pstmt.setInt(6, bVo.getNum());
 			pstmt.executeUpdate();
-			String sql = "insert into board values(bo_seq.NEXTVAL,?,?,?,?,sysdate,?,?,?,0,?)";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, bean.getWriter());
-			pstmt.setString(2, bean.getEmail());
-			pstmt.setString(3, bean.getSubject());
-			pstmt.setString(4, bean.getPassword());
-			pstmt.setInt(5, ref);
-			pstmt.setInt(6, re_step);
-			pstmt.setInt(7, re_level);
-			pstmt.setString(8, bean.getContent());
-
-			pstmt.executeUpdate();
-			con.close();
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
 		}
 	}
 
-	public BoardBean oneupdateBoard(int num) {
-		BoardBean bean = new BoardBean();
-		getCon();
-
+	public BoardVO checkPassWord(String pass, String num) {
+		String sql = "select * from board where pass=? and num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO bVo = null;
 		try {
-			String sql = "select * from board where num=?";
-			pstmt = con.prepareStatement(sql);
-
-			pstmt.setInt(1, num);
-			rs = pstmt.executeQuery();
-
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pass);
+			pstmt.setString(2, num);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				// 테이블에 저장되어 있는 값들 다 가져와서 bean에 세팅
-				bean.setNum(rs.getInt(1));
-				bean.setWriter(rs.getString(2));
-				bean.setEmail(rs.getString(3));
-				bean.setSubject(rs.getString(4));
-				bean.setPassword(rs.getString(5));
-				bean.setReg_date(rs.getDate(6).toString());// 객체를 문자열로
-				bean.setRef(rs.getInt(7));
-				bean.setRe_step(rs.getInt(8));
-				bean.setRe_level(rs.getInt(9));
-				bean.setReadcount(rs.getInt(10));
-				bean.setContent(rs.getString(11));
+				bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setReadcount(rs.getInt("readcount"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
 			}
-			con.close();
-
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return bean;
+		return bVo;
 	}
 
-	public String getPass(int num) {
-		String pass = "";
-		getCon();
+	public void deleteBoard(String num) {
+		String sql = "delete board where num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		try {
-			String sql = "select password from board where num=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, num);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				pass = rs.getString(1);
-			}
-			con.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return pass;
-	}
-	public void updateBoard(BoardBean bean) {
-		getCon();
-		try {
-			String sql="update board set subject=?,content=? where num=?";
-			pstmt=con.prepareStatement(sql);
-			
-			pstmt.setString(1, bean.getSubject());
-			pstmt.setString(2, bean.getContent());
-			pstmt.setInt(3, bean.getNum());
-			
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
 			pstmt.executeUpdate();
-			con.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public void deleteBoard(int num) {
-		getCon();
-		
-		try {
-			String sql="delete from board where num=?";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setInt(1, num);
-			pstmt.executeUpdate();
-			con.close();
-		}catch(Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
